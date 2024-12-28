efc819473342a32016b7956884c8dfa91165c87d
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-12-28 14:45:53 -0300

    fix: homebrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ xcode-select --install
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
+export PATH=/opt/homebrew/bin:$PATH
 
 # Stow
 brew install stow 
