b705a8c03ea01a123da97ecc9f11b4bb77f2b905
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-12-29 10:06:10 -0300

    refactor: improve init script

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Make sure XCode Command Line Tools are installed and up to date
 if xcode-select -p &>/dev/null; then
   echo "✅ XCode Command Line Tools are already installed"
   echo "🔄 Checking for updates to XCode Command Line Tools"
@@ -18,7 +19,35 @@ else
   read
 fi
 
-git clone --recurse-submodules https://github.com/emiliosheinz/dotfiles.git $HOME/dotfiles
+# Configure GitHub's SSH key
+if [ -f "$HOME/.ssh/id_ed25519" ]; then
+  echo "✅ SSH key already exists"
+else
+  echo "🔑 Generating SSH key"
+  echo "Enter your email address:"
+  read email
+  ssh-keygen -t ed25519 -C "$email"
+  eval "$(ssh-agent -s)"
+
+  echo "📝 Creating SSH config"
+  touch $HOME/.ssh/config
+  echo "Host github.com" >> $HOME/.ssh/config
+  echo "  AddKeysToAgent yes" >> $HOME/.ssh/config
+  echo "  UseKeychain yes" >> $HOME/.ssh/config
+  echo "  IdentityFile ~/.ssh/id_ed25519" >> $HOME/.ssh/config
+
+  ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+
+  pbcopy < $HOME/.ssh/id_ed25519.pub
+  echo "📋 SSH key copied to clipboard, paste it in GitHub"
+
+  open https://github.com/settings/ssh/new
+
+  echo "Press enter to continue"
+  read
+fi
+
+git clone --recurse-submodules git@github.com:emiliosheinz/dotfiles.git $HOME/dotfiles
 
 echo ""
 echo "Dotfiles repository cloned to $HOME/dotfiles"
